ac8eaaf363f500fccc647f66d2946aded3c86a2f
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-04-15 21:09:55 +0200

    Initial Commit

---
+++ Bad Habits/Bad_HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bad_HabitsApp.swift
+//  Bad Habits
+//
+//  Created by Carlos Román Alcaide on 15/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Bad_HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bad Habits/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bad Habits
+//
+//  Created by Carlos Román Alcaide on 15/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
